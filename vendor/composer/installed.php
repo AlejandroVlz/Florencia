07fe1e8d7209374490e026578f954ee6514532e4
Alejandro Valdez Endañu <alexvlzm10@gmail.com>
2022-02-12 12:17:28 -0600

    agrgando dependencia

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '7e4fea14f33239c9d5e0793d750d4c50cbaa9ba9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '7e4fea14f33239c9d5e0793d750d4c50cbaa9ba9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
